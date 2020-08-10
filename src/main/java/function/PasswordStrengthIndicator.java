57e67adc47e35b317c187bfbe4678017deadb039
pescarualex <pescarualexflorin@gmail.com>
2020-08-10 14:26:23 +0300

    PasswordStrengthIndicator

---
+++ src/main/java/function/PasswordStrengthIndicator.java
@@ -0,0 +1,39 @@
+package function;
+
+import java.util.Scanner;
+
+public class PasswordStrengthIndicator {
+
+    private void passwordValidator(String password) {
+
+        if (password.matches("(?=.*[0-9]).*")) {
+            System.out.println("The password '" + password + "' is a very week password.");
+        } else if (password.matches("([a-z]+)")) {
+            System.out.println("The password '" + password + "' is a week password.");
+        } else if (password.matches("([0-9]+)") && password.matches(("([a-z]+)"))) {
+            System.out.println("The password '" + password + "' is a strong password.");
+        } else if (password.matches("^(?=.*[a-z])")) {
+            if (password.matches("(?=.*[0-9])")) {
+                if (password.matches("(?=.*[!@#\\$%\\^&\\*])(?=.{8,})")) {
+                    System.out.println("The password '" + password + "' is a very strong password.");
+                }
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        PasswordStrengthIndicator pass = new PasswordStrengthIndicator();
+
+        System.out.println("Enter the password: ");
+        String password = sc.next();
+
+
+
+        pass.passwordValidator(password);
+
+
+    }
+
+}
