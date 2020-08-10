7cc5b6ae564f8aeb2e54513fd9fee8a56a9a79f4
pescarualex <pescarualexflorin@gmail.com>
2020-08-10 14:50:39 +0300

    third attempt

--- src/main/java/function/PasswordStrengthIndicator.java
+++ src/main/java/function/PasswordStrengthIndicator.java
@@ -12,13 +12,9 @@ public class PasswordStrengthIndicator {
             System.out.println("The password '" + password + "' is a week password.");
         } else if (password.matches("([0-9]+)") && password.matches(("([a-z]+)"))) {
             System.out.println("The password '" + password + "' is a strong password.");
-        } else if (password.matches("^(?=.*[a-z])")) {
-            if (password.matches("(?=.*[0-9])")) {
-                if (password.matches("(?=.*[!@#\\$%\\^&\\*])")) {
-                    System.out.println("The password '" + password + "' is a very strong password.");
-                }
+        } else if (password.matches("^(?=.*[a-z])(?=.*[0-9])(?=.*[!@#\\$%\\^&\\*])")) {
+            System.out.println("The password '" + password + "' is a very strong password.");
 
-            }
         }
     }
 
